40d5b2738982d90d0c1f7a4109c09859da030a51
Samrude1 <126822763+Samrude1@users.noreply.github.com>
2025-08-19 22:23:21 +0300

    mongodb säätöä

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Ladataan ympäristömuuttujat config.env-tiedostosta
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -21,7 +26,15 @@ app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
 
 // Käynnistetään palvelin
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
 });
-//notes
+
+// Handle unhandled rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
