4cb22dd4d511dae407f860122f4ba4c58884600b
Samrude1 <126822763+Samrude1@users.noreply.github.com>
2025-09-05 11:10:53 +0300

    courses routing

---
+++ controllers/courses.js
@@ -0,0 +1,27 @@
+const ErrorResponse = require("../utils/errorResponse");
+const Course = require("../models/course");
+const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
+const qs = require("qs");
+
+// @desc    Get all courses
+// @route   GET /api/v1/courses
+// @route   GET /api/v1/bootcamps/:bootcampId/courses
+// @access  Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query;
+
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find();
+  }
+
+  const courses = await query;
+
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
+});
