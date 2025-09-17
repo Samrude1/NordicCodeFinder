d46b6b89aad72efbd2991dfcea27069341d8403f
Sami Rautanen <srautanen77@gmail.com>
2025-09-17 19:11:33 +0300

    lisää authentikointia

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,22 +16,24 @@ const advancedResults = require("../middleware/advancedResults");
 const courseRouter = require("./courses");
 
 const router = express.Router();
+const { protect } = require("../middleware/auth");
 
 // reroute into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
+router.route("/:id/photo").put(protect, bootcampPhotoUpload);
+
 router
   .route("/")
   .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, createBootcamp);
 
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
-router.route("/:id/photo").put(bootcampPhotoUpload);
 module.exports = router;
