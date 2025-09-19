b7309e72ab65f65bdcb7f5fb9dc538db1c43badc
Sami Rautanen <srautanen77@gmail.com>
2025-09-19 12:01:40 +0300

    lisätty roolit 'user' ja 'publisher'

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 // load models
 const Bootcamp = require("./models/bootcamp.js");
 const Course = require("./models/course.js");
+const User = require("./models/user.js");
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI);
@@ -20,12 +21,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 // import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log("Data imported...".green.inverse);
     process.exit();
@@ -39,6 +44,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log("Data destroyed...".red.inverse);
     process.exit();
